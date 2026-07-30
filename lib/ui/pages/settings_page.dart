9b8ede8c245a41e325fa70aa2e797701f167d4d6
a807906271 <807906271@qq.com>
2026-07-30 21:51:45 +0800

    修正目录

--- lib/ui/pages/settings_page.dart
+++ lib/ui/pages/settings_page.dart
@@ -1,8 +1,8 @@
 /// 设置页面
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
-import '../automation/automation_channel.dart';
-import '../automation/automation_session.dart';
+import '../../automation/automation_channel.dart';
+import '../../automation/automation_session.dart';
 
 /// 设置页面
 class SettingsPage extends StatefulWidget {
